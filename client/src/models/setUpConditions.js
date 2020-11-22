9dce6f3b759744dced59cb15bb140a87262d5f96
nikos theodoropoulos <ntheodoropoulos@outlook.com>
2020-11-22 20:37:18 +0200

    added more traps

--- client/src/models/setUpConditions.js
+++ client/src/models/setUpConditions.js
@@ -9,5 +9,14 @@ export const condition = new Condition(
         {x: 3, y: 8},
         {x: 7, y: 4},
         {x: 9, y: 7},
-        {x: 1, y: 4},   
+        {x: 1, y: 4},
+        {x: 1,  y: 3},
+        {x: 1,  y: 8},
+        {x: 2,  y: 4},
+        {x: 2,  y: 9},
+        {x: 4,  y: 8},
+        {x: 3,  y: 3},
+        {x: 6,  y: 9},
+        {x: 8,  y: 8},
+
     ])
